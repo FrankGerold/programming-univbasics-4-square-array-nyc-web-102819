6adbde318b76d9e40a118c3f91c2d866f42b3bd5
Frank Gerold <franketsgerold@gmail.com>
2019-10-11 19:12:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array, power=2)
   counter = 0 
   while array.length < counter  do
-    array[counter]**power
+    array[counter]=array[counter]**power
     counter +=1
   end
 end
