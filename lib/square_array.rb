1513509e1e95f1c62af963a28c8c86067adfbbc9
Frank Gerold <franketsgerold@gmail.com>
2019-10-11 19:18:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array, power = 2)
   counter = 0 
   new_array = []
-  while array.length < counter  do
+  while  < counter  do
     new_array[counter] = array[counter] ** power
     counter += 1
   end
